c10967af767c36bf4a19a13ce2ebd131bad4fa47
Raihan Adam <hallwack.id@gmail.com>
2022-11-07 23:27:47 +0700

    feat(api): add knexfile for migration connection

---
+++ api/knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      host: "127.0.0.1",
+      port: 5432,
+      user: "root",
+      password: "root",
+      database: "gis-final-project",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      host: "127.0.0.1",
+      port: 5432,
+      user: "root",
+      password: "root",
+      database: "gis-final-project",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
