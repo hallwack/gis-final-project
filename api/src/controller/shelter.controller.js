d4d0ecad62e1451273f2b4c85f16741f273307b5
Raihan Adam <hallwack.id@gmail.com>
2022-11-26 10:45:02 +0700

    fix: fixing query to get geojson format

--- api/src/controller/shelter.controller.js
+++ api/src/controller/shelter.controller.js
@@ -1,8 +1,38 @@
-const { knex } = require("../helper/db");
+const { knex, geo } = require("../helper/db");
 
 exports.getAllShelters = async (req, res) => {
   try {
-    const shelters = await knex("shelters");
+    /* const sheltersGeoJSON = await knex.raw(
+      "SELECT JSONB_BUILD_OBJECT('type', 'FeatureCollection', 'features', JSON_AGG(features.feature)) FROM (SELECT row_to_json(inputs) As feature FROM (SELECT 'Feature' As type, ST_AsGeoJSON(c.coordinate)::json As geometry, row_to_json((SELECT c FROM (SELECT id, name) As c)) As properties FROM shelters As c WHERE c.coordinate is not NULL) As inputs) features"
+    ); */
+    const shelters = await knex("shelters").select(
+      "id",
+      "name",
+      geo.asText("coordinate")
+    );
+
+    const featureCollection = {
+      type: "FeatureCollection",
+      features: shelters.map((row) => {
+        return {
+          type: "Feature",
+          geometry: {
+            type:
+              row.coordinate.slice(0, 5).charAt(0).toUpperCase() +
+              row.coordinate.slice(1, 5).toLowerCase(),
+            coordinates: row.coordinate
+              .slice(6, row.coordinate.length - 1)
+              .split(" ")
+              .map((c) => Number.parseFloat(c)),
+          },
+          properties: {
+            id: row.id,
+            name: row.name,
+          },
+        };
+      }),
+    };
+
     return res.json({
       success: true,
       message: "Get All Shelters Successfully",
