8b86fd4b45a2a05911f17c6bbe88152db90c4859
Raihan Adam <hallwack.id@gmail.com>
2022-12-31 20:55:03 +0700

    fix: fixing route controller

--- api/src/controller/route.controller.js
+++ api/src/controller/route.controller.js
@@ -1,38 +1,42 @@
-const { knex, geo } = require('../helper/db')
+const { knex, geo } = require("../helper/db");
 
 exports.getAllRoutes = async (req, res) => {
   try {
     const routes = await knex("routes")
-      .join('routes_geometry', 'routes.geometry', 'routes_geometry.id').select('*', geo.asText('geom'))
+      .join("routes_geometry", "routes.geometry", "routes_geometry.id")
+      .select("*", geo.asText("geom"));
 
-    const coordinates = routes.map(route => {
+    const coordinates = routes.map((route) => {
       let filteredRoute = [];
-      let r = route.geom.slice(11, route.geom.length - 1).split(",")
-      r.map(item => filteredRoute.push(item.split(' ').map(i => parseFloat(i))))
-      return filteredRoute
-    }
-    )
+      let r = route.geom.slice(11, route.geom.length - 1).split(",");
+      r.map((item) =>
+        filteredRoute.push(item.split(" ").map((i) => parseFloat(i)))
+      );
+      return filteredRoute;
+    });
 
     const featureCollection = {
       type: "FeatureCollection",
       features: routes.map((row) => {
-        let filteredRoute = []
-        let r = row.geom.slice(11, row.geom.length - 1).split(",")
-        r.map(item => filteredRoute.push(item.split(' ').map(i => parseFloat(i))))
+        let filteredRoute = [];
+        let r = row.geom.slice(11, row.geom.length - 1).split(",");
+        r.map((item) =>
+          filteredRoute.push(item.split(" ").map((i) => parseFloat(i)))
+        );
         return {
           type: "Feature",
           geometry: {
             type: "LineString",
-            coordinates: filteredRoute
+            coordinates: filteredRoute,
           },
           properties: {
             id: row.id,
             code: row.code,
+            description: row.description,
           },
         };
       }),
-    }
-
+    };
 
     /* const rute1 = routes.map(rute => rute.coordinate
       .slice(6, rute.coordinate.length - 1)
@@ -57,4 +61,60 @@ exports.getAllRoutes = async (req, res) => {
       message: `Error: ${err.message}`,
     });
   }
-}
+};
+
+exports.getRoutesById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const routes = await knex("routes")
+      .join("routes_geometry", "routes.geometry", "routes_geometry.id")
+      .select("*", geo.asText("geom"))
+      .where("routes_geometry.id", id);
+
+    const featureCollection = {
+      type: "FeatureCollection",
+      features: routes.map((row) => {
+        let filteredRoute = [];
+        let r = row.geom.slice(11, row.geom.length - 1).split(",");
+        r.map((item) =>
+          filteredRoute.push(item.split(" ").map((i) => parseFloat(i)))
+        );
+        return {
+          type: "Feature",
+          geometry: {
+            type: "LineString",
+            coordinates: filteredRoute,
+          },
+          properties: {
+            id: row.id,
+            code: row.code,
+          },
+        };
+      }),
+    };
+
+    return res.json({
+      success: true,
+      message: "Get Route By Id Successfully",
+      results: { geom: featureCollection, info: routes },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: `Error: ${err.message}`,
+    });
+  }
+};
+
+exports.updateShelterData = async (req, res) => {
+  const { id } = req.params;
+  const { arrival_id, departure_id, description } = req.body;
+  try {
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: `Error: ${error.message}`,
+    });
+  }
+};
