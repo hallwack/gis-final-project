d119e440edc93c7ea6f9bba5e3362154ce54163f
Raihan Adam <hallwack.id@gmail.com>
2022-12-31 20:55:21 +0700

    feat: add update controller

--- api/src/routes/route.route.js
+++ api/src/routes/route.route.js
@@ -2,5 +2,6 @@ const routes = require("express").Router();
 const routeController = require("../controller/route.controller");
 
 routes.get("/", routeController.getAllRoutes);
+routes.get("/:id", routeController.getRoutesById);
 
 module.exports = routes;
