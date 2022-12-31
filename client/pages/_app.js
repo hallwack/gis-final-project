229d294ced14adcc37bd6c9f685f3110b98bd3a5
Raihan Adam <hallwack.id@gmail.com>
2022-12-31 20:56:24 +0700

    fix: add provider in app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,18 @@
-import { Inter } from '@next/font/google';
-import '../styles/globals.css';
+import { Inter } from "@next/font/google";
+import { Provider } from "react-redux";
+import { store } from "../store";
+import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
-  )
+    <Provider store={store}>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
