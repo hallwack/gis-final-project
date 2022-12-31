954153f4a6a149519df3d3e9c39446e068a6a5b9
Raihan Adam <hallwack.id@gmail.com>
2022-12-31 20:55:36 +0700

    feat: add get by id and post shelter

--- api/src/routes/shelter.route.js
+++ api/src/routes/shelter.route.js
@@ -2,5 +2,7 @@ const shelters = require("express").Router();
 const shelterController = require("../controller/shelter.controller");
 
 shelters.get("/", shelterController.getAllShelters);
+shelters.get("/:id", shelterController.getShelterById);
+shelters.post("/:id", shelterController.updateShelterData);
 
 module.exports = shelters;
